49b96837dac755550117d0897d39646688d97291
nataliagilewska <n.gilewska@tt.com.pl>
2019-05-08 15:53:19 +0200

    zad1

--- src/Ksiazka.java
+++ src/Ksiazka.java
@@ -16,13 +16,13 @@ public class Ksiazka {
     boolean czyPrzeczytana(int przeczytane)
     {
        int pom =iloscStron-przeczytane;
-       iloscPrzeczytanychStron+=przeczytane;
+       iloscPrzeczytanychStron+=przeczytane; //to zwiekszy ilosc stron tylko dla obiektu na ktorym wywolalam metode, np dla ksiazki1, a w ksiazce 2 dalej zostanie 0
        if (pom==0) return true;
        else return false;
     }
     void czytaj(int ileStron)//ma dodawac te strony do ilosci przeczytanych stron
     {
-    
+
     iloscPrzeczytanychStron+=ileStron;
 
     }
