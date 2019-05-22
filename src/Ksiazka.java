6aa8737440a65582f1ca025b30b3aa4da91857f3
nataliagilewska <n.gilewska@tt.com.pl>
2019-05-22 11:51:20 +0200

    zadanka

--- src/Ksiazka.java
+++ src/Ksiazka.java
@@ -3,6 +3,7 @@ public class Ksiazka {
     private String autor = "Pan Autor";
     private int iloscStron = 1600;
     private int iloscPrzeczytanychStron=0;
+    int iloscPozostalychStron = 1600;
 
     Ksiazka()
     {
@@ -10,20 +11,22 @@ public class Ksiazka {
     }
     void coToZaKsiazka()
     {
-    System.out.println(tytul);
+    System.out.println("Tytul: "+ tytul+" Autor: "+ autor);
+    }
+    void czytaj(int ileStron)
+    {
+        iloscPozostalychStron = iloscStron-ileStron;
+        if (iloscPozostalychStron<0)
+        {
+            iloscPozostalychStron=0;
+        }
     }
 
-    boolean czyPrzeczytana(int przeczytane)
+    boolean czyPrzeczytana()
     {
-       int pom =iloscStron-przeczytane;
-       iloscPrzeczytanychStron+=przeczytane; //to zwiekszy ilosc stron tylko dla obiektu na ktorym wywolalam metode, np dla ksiazki1, a w ksiazce 2 dalej zostanie 0
-       if (pom==0) return true;
+       if (iloscPozostalychStron==0) //to zwiekszy ilosc stron tylko dla obiektu na ktorym wywolalam metode, np dla ksiazki1, a w ksiazce 2 dalej zostanie 0
+       return true;
        else return false;
     }
-    void czytaj(int ileStron)//ma dodawac te strony do ilosci przeczytanych stron
-    {
 
-    iloscPrzeczytanychStron+=ileStron;
-
-    }
 }
--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,23 @@ public class Main {
         //ZAD 2.
         Ksiazka ksiazka1=new Ksiazka();
         ksiazka1.coToZaKsiazka();
+        ksiazka1.czytaj(5);
+        System.out.println("Ilosc pozostalych stron: "+ksiazka1.iloscPozostalychStron);
+        if (ksiazka1.czyPrzeczytana()==true)
+        {
+            System.out.println("Ksiazka przeczytana");
+        }
+        else
+            System.out.println("Ksiazka nieprzeczytana");
+
+
+        //Zad 3
+
+        Samochod samochod1 = new Samochod("Fiat","Czerwony",5.20,200);
+        //System.out.println("Paliwo samochod1: " +samochod1.iloscPaliwa);
+        samochod1.ilePaliwa();
+        samochod1.tankuj(5);
+        samochod1.jedz(1);
 
 
     }
